2c5630a4cc49b0c97166745a4cbb95b8b13ecbb9
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-02-12 21:18:59 +0800

    Create User.cs
    created user model

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace furever_home.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string? MiddleInitial { get; set; }
+        public string LastName { get; set; } = String.Empty;
+        [Phone]
+        public string ContactNo {  get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public bool IsAdmin { get; set; }
+        public string Address { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
