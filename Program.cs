d94c224d9b44c3f35d2fcdea6e474d7435fe504f
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-04-16 13:50:53 +0800

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -18,12 +18,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
