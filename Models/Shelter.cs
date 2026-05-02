6f4720c262ceb03b81f298a0123ddead5a2bbcbb
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-05-02 22:03:33 +0800

    Created Shelter Model

---
+++ Models/Shelter.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FureverHome.Models
+{
+    public class Shelter
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
