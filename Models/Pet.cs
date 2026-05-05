0eef02184e54f38e51c4a64d578bf0eee6358fc6
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-05-05 08:59:50 +0800

    Created Pet model

---
+++ Models/Pet.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using FureverHome.Enums;
+
+namespace FureverHome.Models
+{
+    public class Pet
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+      
+        [BsonRepresentation(BsonType.String)]
+        public PetType PetType { get; set; }
+      
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Gender Gender { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string ShelterId { get; set; }
+      
+        [BsonRepresentation(BsonType.String)]
+        public PetStatus Status { get; set; } = PetStatus.Available;
+      
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
