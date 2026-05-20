8ed8fc3746cadd5c3230f0f15ed0a76e8c4eb9ef
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-05-20 09:00:57 +0800

    Created RegisterViewModel

---
+++ ViewModel/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace furever_home.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
