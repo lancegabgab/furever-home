91346834b1bbab1715b8e38cea547e0704c3fbc3
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-06-16 19:24:50 +0800

    Created DbContext

---
+++ Data/FureverHomeContext.cs
@@ -0,0 +1,16 @@
+﻿using FureverHome.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace furever_home.Data
+{
+    public class FureverHomeContext : DbContext
+    {
+        public FureverHomeContext(DbContextOptions<FureverHomeContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Shelter> Shelters { get; set; }
+    }
+}
