ada21cce444b068636580b1bbbb528897e7847df
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-06-22 15:25:08 +0800

    Updated HomeController
    Added [AllowAnonymous] attributes to actions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,36 +1,49 @@
 using System.Diagnostics;
 using FureverHome.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FureverHome.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult About()
         {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult Contact()
         {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult Terms()
         {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult Donate()
         {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult FAQ()
         {
             return View();
