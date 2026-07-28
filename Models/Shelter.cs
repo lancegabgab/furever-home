03d4123200384910317386cc5425a15fe56a9859
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-07-28 19:29:22 +0800

    Added ImageUrl in Shelter Model

--- Models/Shelter.cs
+++ Models/Shelter.cs
@@ -11,5 +11,7 @@ namespace FureverHome.Models
         [Required]
         [MaxLength(100)]
         public string Name { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
     }
 }
