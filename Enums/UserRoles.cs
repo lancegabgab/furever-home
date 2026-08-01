fd21c7e2cd7ce2fdf37caacbf39fb93c73f7a1c8
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-01 10:46:34 +0800

    Created UserRoles enum

---
+++ Enums/UserRoles.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FureverHome.Enums
+{
+    public enum UserRoles
+    {
+        [Display(Name = "Adopter")]
+        Adopter = 1,
+
+        [Display(Name = "Shelter Admin")]
+        ShelterAdmin = 2,
+
+        [Display(Name = "Shelter Staff")]
+        ShelterStaff = 3,
+
+        [Display(Name = "Super Admin")]
+        SuperAdmin = 4
+    }
+}
