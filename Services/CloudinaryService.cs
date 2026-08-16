9b7e702248cde2eef8da50f3f96387d19548493c
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-16 08:53:46 +0800

    Setup Cloudinary

---
+++ Services/CloudinaryService.cs
@@ -0,0 +1,58 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace FureverHome.Services;
+
+public class CloudinaryService : ICloudinaryService
+{
+    private readonly Cloudinary _cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary)
+    {
+        _cloudinary = cloudinary;
+    }
+
+    public async Task<ImageUploadResult> UploadImageAsync(
+        IFormFile file,
+        string folder)
+    {
+        if (file == null || file.Length == 0)
+        {
+            throw new ArgumentException("No image was provided.");
+        }
+
+        await using var stream = file.OpenReadStream();
+
+        var uploadParams = new ImageUploadParams
+        {
+            File = new FileDescription(file.FileName, stream),
+            Folder = folder,
+            UseFilename = true,
+            UniqueFilename = true,
+            Overwrite = false
+        };
+
+        var result = await _cloudinary.UploadAsync(uploadParams);
+
+        if (result.Error != null)
+        {
+            throw new Exception(result.Error.Message);
+        }
+
+        return result;
+    }
+
+    public async Task<bool> DeleteImageAsync(string publicId)
+    {
+        if (string.IsNullOrWhiteSpace(publicId))
+        {
+            return false;
+        }
+
+        var deleteParams = new DeletionParams(publicId);
+
+        var result = await _cloudinary.DestroyAsync(deleteParams);
+
+        return result.Result == "ok";
+    }
+}
---
+++ Services/ICloudinaryService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace FureverHome.Services
+{
+    public interface ICloudinaryService
+    {
+        Task<ImageUploadResult> UploadImageAsync( IFormFile file, string folder);
+
+        Task<bool> DeleteImageAsync(string publicId);
+    }
+}
